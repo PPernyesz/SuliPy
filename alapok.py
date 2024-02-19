596c2c69506d1ae2dc73c30f047913b1ef7bc106
PPernyesz <49074781+PPernyesz@users.noreply.github.com>
2024-02-19 16:44:49 +0100

    Add files via upload

---
+++ alapok.py
@@ -0,0 +1,22 @@
+# A karaktersorozatok (un. sztringek) megadásakor használhatunk aposztrófot vagy idézőjelet, mind a kettő használata megengedett.
+
+print("Apple tree")
+print('Apple tree')
+
+# Kommentek írása
+# Programunkban elhelyezhetünk megjegyzéseket, kommenteket.
+
+''' 
+Többsoros kommentet írhatunk
+3 aposztróf vagy 3 idézőjel közé írva.
+Így szoktuk megadni általában a program vagy programrészlet leírását.
+'''
+
+""" 
+Többsoros kommentet írhatunk
+3 aposztróf vagy 3 idézőjel közé írva.
+Így szoktuk megadni általában a program vagy programrészlet leírását.
+"""
+
+print("Apple tree") # Ez marad így
+# print('Apple tree')
---
+++ valtozok.py
@@ -0,0 +1,11 @@
+# Változó értékének megadása
+
+szam = 7
+szo = 'apple'
+szo2 = "grape"
+
+# Változó értékének kiíratása képernyőre
+
+print(szam)
+print(szo)
+print(szo2)
