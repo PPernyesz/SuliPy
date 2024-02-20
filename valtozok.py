23c2b158bb77cc1fc2ef1ee57e8bbc3431c5c6a9
PPernyesz <pernyo@gmail.com>
2024-02-20 13:23:05 +0100

    New variable added

--- valtozok.py
+++ valtozok.py
@@ -1,12 +1,14 @@
 # Változó értékének megadása
 
 szam = 7
+szam2 = szam + 3
 szo = 'apple'
 szo2 = "grape"
 
 # Változó értékének kiíratása képernyőre
 
 print(szam)
+print(szam2)
 print(szo)
 print(szo2)
 
